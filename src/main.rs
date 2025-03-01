371981bb9f843d6b95d4dab5128eaa06755a9d95
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-01 17:11:53 +0700

    input nilai matematika

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // Input nama siswa
     println!("Masukan Nama siswa :");
@@ -5,4 +7,12 @@ fn main() {
     io::stdin()
         .read_line(&mut nama)
         .expect("gagal membaca input");
+
+    // Input nilai matematika
+    println!("Masukan nilai matematika :");
+    let mut matematika = String::new();
+    io::stdin()
+        .read_line(&mut matematika)
+        .expect("Gagal membaca input");
+    let matematika: f64 = matematika.trim().parse().expect("Input bukan angka");
 }
