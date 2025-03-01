16b730094934d2f57b3d42dae488f5860b86321a
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-01 17:06:59 +0700

    input name student

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // Input nama siswa
+    println!("Masukan Nama siswa :");
+    let mut nama = String::new();
+    io::stdin()
+        .read_line(&mut nama)
+        .expect("gagal membaca input");
 }
